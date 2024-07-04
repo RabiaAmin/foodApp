24fbbcce3c417ca9fe31a9851565ec4c96bd1a35
rabia <rabia10march@gmail.com>
2024-07-04 20:45:53 +0500

    screen 1 ui completed

---
+++ lib/constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF75A47F);
+const kPrimaryLightColor = Color(0xFFFCFFE0);
+const kAccrent1Color = Color(0xFFF5DAD2);
+const kAccrent2Color = Color(0xFFBACD92);
